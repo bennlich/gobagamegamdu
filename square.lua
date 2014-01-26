90ee5d0072e6c1b6da40f2b0d953f665c4d0b578
Ben Goldsmith <bengold@stanford.edu>
2014-01-25 17:09:19 -0800

    adding elevation to squares

--- square.lua
+++ square.lua
@@ -1,10 +1,11 @@
 Class = require("libs.hump.class")
 
 Square = Class{
-  init = function(self, pos, size, color)
+  init = function(self, pos, size, color, elevation)
     self.pos = (vector.isvector(pos) and pos) or vector(unpack(pos))
     self.size = size
     self.color = color
+    self.elevation = elevation or 0
   end
 }
 
@@ -17,7 +18,7 @@ function Square:draw(camera)
   love.graphics.push()
   local groundPos = camera:transformCoords(self.pos.x, self.pos.y)
   -- draw the character from the head down
-  local headY = groundPos.y - self.size
+  local headY = groundPos.y - self.size - self.elevation
   love.graphics.rectangle("fill", groundPos.x, headY, self.size, self.size)
   love.graphics.pop()
 end
