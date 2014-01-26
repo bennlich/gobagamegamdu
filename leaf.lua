1ecc8776989304e490c035f81d214cff42da8cb6
bennlich@gmail.com <bennlich@gmail.com>
2014-01-25 22:47:51 -0800

    added trees.

---
+++ leaf.lua
@@ -0,0 +1,48 @@
+require("square")
+
+Class = require("libs.hump.class")
+vector = require("libs.hump.vector")
+
+Leaf = Class{__includes=Square,
+	init = function(self, tree, posOffset, size, elevationOffset, numFruits)
+		
+		local leafColor = { 0, 200, 50 }
+
+		Square.init(self, {
+      pos = tree.pos+posOffset,
+      size = size,
+      color = leafColor,
+      elevation = tree.elevation+tree.trunkHeight+elevationOffset
+    })
+
+    --init fruits
+		local minNumFruits = math.floor(numFruits)
+		local probFruit = numFruits % minNumFruits
+		local numFruits = minNumFruits + ((math.random() > probFruit and 1) or 0)
+    local fruitColor = { 200, 200, 20 }
+		self.fruits = {}
+		for i=1,numFruits do
+			self.fruits[i] = Square({
+        pos = self.pos + vector(math.random(-self.size/2, self.size/2), 0),
+        size = 5,
+        color = fruitColor,
+        elevation = self.elevation+math.random(self.size)
+      })
+		end
+	end
+}
+
+function Leaf:update(dt)
+	table.foreach(self.fruits, function(k, fruit)
+		fruit:update(dt)
+	end)
+end
+
+function Leaf:draw(camera)
+  Square.draw(self, camera)
+	if (self.fruits) then
+		table.foreach(self.fruits, function(k, fruit)
+			fruit:draw(camera)
+		end)
+	end
+end
---
+++ tree.lua
@@ -0,0 +1,60 @@
+require("leaf")
+require("loadable")
+
+Class = require("libs.hump.class")
+vector = require("libs.hump.vector")
+
+Tree = Class{__includes=Loadable,
+  defaults = {
+    pos = vector(0,0),
+    fruitsPerLeaf = 2.5,
+    size = 1,
+    elevation = 0,
+    trunkHeight = 100,
+    trunkWidth = 10,
+    color = {200, 100, 50},
+    thickness = 10,
+    name = "missingno"
+  },
+  init = function(self, opts)
+    Loadable.init(self, opts)
+    print('tree pos')
+    print(self.pos:unpack())
+    --default opts
+    local fruitsPerLeaf = (opts and opts.fruitsPerLeaf) or 2.5
+    self.trunkHeight = self.size * self.trunkHeight
+    self.trunkWidth = self.size * self.trunkWidth
+    --init leaves
+    self.leaves = {
+      Leaf(self, vector(self.trunkWidth/2,0),
+        self.size*100, (1/2)*self.size*100, fruitsPerLeaf),
+      Leaf(self, vector(-(1/2)*self.trunkWidth-(1/4)*self.size*110,0),
+        self.size*110, (1/4)*self.size*110, fruitsPerLeaf),
+      Leaf(self, vector(self.trunkWidth+(1/2)*self.size*80,0),
+        self.size*80, 0, fruitsPerLeaf),
+      Leaf(self, vector(-self.trunkWidth/2,0),
+        self.size*60, (7/3)*self.size*60, fruitsPerLeaf)
+    }
+  end
+}
+
+function Tree:update(dt)
+  table.foreach(self.leaves, function(k, leaf)
+    leaf:update(dt)
+  end)
+end
+
+function Tree:draw(camera)
+  table.foreach(self.leaves, function(k, leaf)
+    leaf:draw(camera)
+  end)
+
+  --draw trunk
+  love.graphics.setColor(self.color)
+  love.graphics.push()
+  local groundPos = camera:transformCoords(self.pos.x - self.trunkWidth/2, self.pos.y)
+  local headY = groundPos.y - self.trunkHeight
+  love.graphics.rectangle("fill", groundPos.x, headY, self.trunkWidth, self.trunkHeight)
+  love.graphics.pop()
+end
+
