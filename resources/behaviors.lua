4a47199cc37d2776bb747b4d6ce86cd146d0b351
Ben Goldsmith <bengold@stanford.edu>
2014-01-26 20:38:59 -0800

    added a dog that follows you

--- resources/behaviors.lua
+++ resources/behaviors.lua
@@ -1,9 +1,13 @@
 behaviors = {}
 
+-- Requires: 
+-- other.xVel: The speed at which the object travels
 function behaviors.moveLeft(dt, obj, scene)
   obj.pos.x = obj.pos.x - obj.other.xVel
 end
 
+-- Requires: 
+-- none
 function behaviors.wrapX(dt, obj, scene )
   local edgeOffset = camera:getEdgeOffset(obj.pos.y)
   local scaledSize = camera:getScale(obj.pos.y)*obj.size
@@ -16,5 +20,17 @@ function behaviors.wrapX(dt, obj, scene )
     obj.pos.x = edgeOffset - scaledSize/2
     scene:collided(obj, {name="edgeRight"}) 
     if obj.originalLabelContent then obj.label:setContent(obj.originalLabelContent) end
+  end
+end
+
+-- Requires: 
+-- other.target: The name of the object in the scene to follow
+-- other.followDist: Follow until you get to this distance
+-- other.maxVel: The speed at which to follow
+function behaviors.follow(dt, obj, scene )
+  local target = scene.objects[obj.other.target]
+  if target.pos:dist(obj.pos) > obj.other.followDist then
+    local vel = obj.other.maxVel*(target.pos-obj.pos):normalized()
+    obj.pos = obj.pos + vel
   end
 end
